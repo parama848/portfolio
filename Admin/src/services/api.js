bb6fb08211fae39ccf33c752de1f66c940009af8
parama848 <parama8148497159@gmail.com>
2026-07-10 18:05:57 +0530

    backend url updated

--- Admin/src/services/api.js
+++ Admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://portfolio-paranthaman-zuzs.onrender.com",
 });
 
 export default API;
